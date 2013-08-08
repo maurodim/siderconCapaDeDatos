249ed40a43f2d14daa21512e01030e54361030e8
hernan <hernan@192.168.0.63>
2013-08-08 18:08:34 -0300

    CORRECCION EN FALLO DEL BUCLE DE REVISION CON LA BASE TANGO

--- src/actualizaciones/ActualizarATango.java
+++ src/actualizaciones/ActualizarATango.java
@@ -4,10 +4,26 @@
  */
 package actualizaciones;
 
+import interfaces.Ideable;
+import java.sql.Connection;
+import java.util.ArrayList;
+import proceso.Coneccion;
+
 /**
  *
  * @author mauro di
  */
-public class ActualizarATango {
+public class ActualizarATango implements Ideable{
+    private Connection con;
+    private Coneccion cone;
+    private Integer numeroMy;
+    private Integer Id_gva03;
+    private ArrayList items=new ArrayList();
+    
+    @Override
+    public void ActualizarIds() {
+        String sql="select pedidos_carga1.numero from pedidos_carga1 where ID_GVA03=0";
+        
+    }
     
 }
