b87fde230d267591fdb8bc10c1d273e38737842a
hernan <hernan@192.168.0.63>
2013-08-08 10:20:09 -0300

    CORRECCION EN FALLO DEL BUCLE DE REVISION CON LA BASE TANGO

--- src/objetos/Mail.java
+++ src/objetos/Mail.java
@@ -5,12 +5,17 @@
 package objetos;
 
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  *
@@ -20,12 +25,30 @@ public class Mail {
     private final Properties propiedades=new Properties();
     private String password="Sider001";
     private Session sesion;
+    private String direccionFile;
+    private String detalleListado;
+    private String asunto;
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    
+    public void setDetalleListado(String detalleListado) {
+        this.detalleListado = detalleListado;
+    }
+    
+    
+    public void setDireccionFile(String direccionFile) {
+        this.direccionFile = direccionFile;
+    }
+    
     private void init(){
         propiedades.put("mail.smtp.host","mail.sidercon.com");
         propiedades.put("mail.smtp.starttls.enable","true");
-        propiedades.put("mail.smtp.pot",25);
-        propiedades.put("mail.smtp.mail.sender","web@sidercon.com");
-        propiedades.put("mail.smtp.user","web@sidercon.com");
+        propiedades.put("mail.smtp.pot",587);
+        propiedades.put("mail.smtp.mail.sender","administracion@sidercon.com");
+        propiedades.put("mail.smtp.user","administracion@sidercon.com");
         propiedades.put("mail.smtp.auth","true");
         sesion=Session.getDefaultInstance(propiedades);
         
@@ -35,9 +58,18 @@ public class Mail {
         try{
             MimeMessage mensaje=new MimeMessage(sesion);
             mensaje.setFrom(new InternetAddress((String)propiedades.get("mail.smtp.mail.sender")));
-            mensaje.addRecipient(Message.RecipientType.TO,new InternetAddress("hernangonzalez@sidercon.com"));
-            mensaje.setSubject("CARGA COMPLETA");
-            mensaje.setText("El reparto del vehiculo esta cerrado para el reparto. Motivo :CAPACIDAD DE CARGA COMPLETADA");
+            mensaje.addRecipient(Message.RecipientType.TO,new InternetAddress("web@sidercon.com"));
+            mensaje.setSubject(asunto);
+            BodyPart texto=new MimeBodyPart();
+            texto.setText("LPM GENERADA, LA MISMA SE ENCUENTRA GUARDADA EN LA CARPETA Server/Ventas/LPM   \n Saludos");
+            BodyPart adjunto=new MimeBodyPart();
+            adjunto.setDataHandler(new DataHandler(new FileDataSource(direccionFile)));
+            adjunto.setFileName(detalleListado);
+            MimeMultipart multiParte=new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            multiParte.addBodyPart(adjunto);
+            //mensaje.setText("El reparto del vehiculo esta cerrado para el reparto. Motivo :CAPACIDAD DE CARGA COMPLETADA");
+            mensaje.setContent(multiParte);
             Transport t=sesion.getTransport("smtp");
             t.connect((String)propiedades.get("mail.smtp.user"), password);
             t.sendMessage(mensaje,mensaje.getAllRecipients());
